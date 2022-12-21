9737ca31c2c98e95c206271ca8c5b565868e31c1
CleanupS20 <fjtobonb@gmail.com>
2022-12-20 23:23:48 -1000

    Finalized Read-Me

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,19 @@
 INSERT INTO department (id, department_name)
-VALUES  (1, "Finance"), 
-        (2, "Sales"), 
-        (3, "Ecommerce"), 
-        (4, "Operations"), 
-        (5, "Manufacturing");
+VALUES  (1, "Human Resources"), 
+        (2, "Customer Support"), 
+        (3, "Sales"), 
+        (4, "Research and Development"), 
+        (5, "Management");
 
 INSERT INTO employee_role (id, title, salary, department_id)
-VALUES  (1, "CFO", 110000, 1), 
-        (2, "Controller", 90000, 1), 
-        (3, "COO", 105000, 4),
-        (4, "Glassblower", 60000, 5);
+VALUES  (1, "CEO", 210000, 5), 
+        (2, "Human Resource Speciallist", 90000, 1), 
+        (3, "Sales Associate", 105000, 3),
+        (4, "Software Engineer", 60000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (1, "Maddie", "Smith", 3, 1),
-        (2, "Taylor", "Turnip", 1, 1),
+VALUES  (1, "Daniel", "Jones", 3, 1),
+        (2, "Travis", "Hunt", 1, 1),
         (3, "Amanda", "Brown", 4, 4),
         (4, "Nick", "Jones", 4, 1),
-        (5, "Tyler", "Mercury", 1, 1);
+        (5, "Tyler", "Murphy", 1, 1);
